79d8c926c657e703a12198f518b4e27f02781cc0
Alexander Georgiev <ageorgiev@appolica.com>
2024-03-14 11:52:17 +0200

    removed some more files

--- lib/domain/model/cocktail/cocktail.dart
+++
@@ -1,9 +0,0 @@
-class Cocktail {
-  const Cocktail({
-    required this.idDrink,
-    required this.strDrink,
-  });
-
-  final String idDrink;
-  final String strDrink;
-}
--- lib/domain/model/cocktail/cocktail_list.dart
+++
@@ -1,9 +0,0 @@
-import 'cocktail.dart';
-
-class CocktailList {
-  const CocktailList({
-    required this.drinks,
-  });
-
-  final List<Cocktail> drinks;
-}
